977956bca33145eeaecb49c9898108c54b42a6d1
Darya Litvintseva <litv.daria@gmail.com>
2018-05-24 13:43:44 +0300

    Sizeof(char) and useless size are fixed

---
+++ file.c
@@ -0,0 +1,310 @@
+//
+// Created by darya on 22.04.18.
+//
+
+#include "file.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <math.h>
+#include <assert.h>
+#include <ares_build.h>
+
+extern int sock;
+const int block_size = 16;
+const int tree_size = 256;
+
+int writeBlock(char* bytes, FILE* save_file) {
+    char buffer[block_size];
+    memset(buffer, 0, block_size);
+    memcpy(buffer, bytes, strlen(bytes));
+    return fwrite(buffer, 1, block_size, save_file);
+}
+
+int min(int a, int b) {
+    if(a < b) {
+        return a;
+    }
+    return b;
+}
+
+char* GetFromFile(int begin, int end) {
+    char* res = (char*)malloc(end - begin + 1);
+    FILE* save_file = fopen("save_file.txt", "r");
+    fseek(save_file, begin, SEEK_SET);
+    fread(res, 1, end - begin, save_file);
+    fclose(save_file);
+    res[end - begin] = '\0';
+    return res;
+}
+
+char* ReadBlock(int* pos) {
+    char* res = (char*)malloc(block_size + 1);
+    FILE* save_file = fopen("save_file.txt", "r");
+    fseek(save_file, *pos, SEEK_SET);
+    fread(res, 1, block_size, save_file);
+    fclose(save_file);
+    res[block_size] = '\0';
+    return res;
+}
+
+char* Concatenate(char* a, char* b) {
+    char* res = (char *)malloc(1 + strlen(a)+ strlen(b));
+    strcpy(res, a);
+    strcat(res, b);
+    free(a);
+    free(b);
+
+    return res;
+}
+
+char* ReadTree(struct file** files, int size) {
+    char* res = NULL;
+    for(int i = 0; i < size; ++i) {
+        if(res == NULL) {
+            res = ReadBlock(&files[i]->start_pos_content);
+        } else {
+            res = Concatenate(res, ReadBlock(&files[i]->start_pos_content));
+        }
+    }
+    return res;
+}
+
+char* ReadLevelTree(int level, struct block1024* parent) {
+    char* res = NULL;
+    for (int i = 0; i < parent->size; ++i) {
+        if(level == 1) {
+            parent->children[i]->children = NULL;
+            if (res == NULL) {
+                res = ReadTree(parent->children[i]->files, parent->children[i]->size);
+            } else {
+                res = Concatenate(res, ReadTree(parent->children[i]->files, parent->children[i]->size));
+            }
+        } else {
+            if (res == NULL) {
+                res = ReadLevelTree(level - 1, parent->children[i]);
+            } else {
+                res = Concatenate(res, ReadLevelTree(level - 1, parent->children[i]));
+            }
+        }
+    }
+    return res;
+}
+
+char* GetContentNew(struct fileTree* file) {
+    int size = min(12, file->size);
+    char* res = ReadTree(file->files, size);
+
+    if (file->size > 12) {
+        res = Concatenate(res, ReadTree(file->block_tree[0]->files, file->block_tree[0]->size));
+        for (int i = 1; i < file->size - 12; ++i) {
+            res = Concatenate(res, ReadLevelTree(i, file->block_tree[i]));
+        }
+    }
+
+    return res;
+}
+
+char* GetName(struct file* file) {
+    return ReadBlock(&file->start_pos_name);
+}
+
+void DestructorFile(struct file* sample, int* pos) {
+    AppendFreeRanges(*pos);
+    free(sample);
+}
+
+void DeleteTree(struct file** files, int size) {
+    for(int i = 0; i < size; ++i) {
+        DestructorFile(files[i], &files[i]->start_pos_content);
+    }
+}
+
+void DeleteLevelTree(int level, struct block1024* parent) {
+    for (int i = 0; i < parent->size; ++i) {
+        if (level == 1) {
+            DeleteTree(parent->children[i]->files, parent->children[i]->size);
+            free(parent->children[i]);
+        } else {
+            DeleteLevelTree(level - 1, parent->children[i]);
+        }
+    }
+    free(parent);
+}
+
+void DestructorFileTree(struct fileTree* sample) {
+    int size = min(12, sample->size);
+
+    DeleteTree(sample->files, size);
+    free(sample->files);
+
+    if (sample->size > 12) {
+        DeleteTree(sample->block_tree[0]->files, sample->block_tree[0]->size);
+        for (int i = 1; i < sample->size - 12; ++i) {
+            DeleteLevelTree(i, sample->block_tree[i]);
+        }
+    }
+
+    DestructorFile(sample->file_name, &sample->file_name->start_pos_name);
+    free(sample);
+}
+
+struct file* WriteBlock(char* block, int* pos) {
+    FILE* save_file;
+    *pos = ClosestPos();
+    if (*pos == -1) {
+        save_file = fopen("save_file.txt", "a");
+        *pos = ftell(save_file);
+    } else {
+        save_file = fopen("save_file.txt", "r+");
+    }
+
+    if (save_file == NULL) {
+        char msg[] = "Error opening file! \n";
+        send(sock, msg, sizeof(msg), 0);
+//        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    struct file* res = (struct file*)malloc(sizeof(struct file));
+    fseek(save_file, *pos, SEEK_SET);
+    writeBlock(block, save_file);
+//    fwrite(block, sizeof(char), strlen(block), save_file);
+    fclose(save_file);
+
+    return res;
+}
+
+void FillTree(struct file*** files, int* blocks, char* buff) {
+    *files = (struct file **) malloc(min(*blocks, tree_size) * sizeof(struct file *));
+
+    for (int i = 0; i < min(*blocks, tree_size); ++i) {
+        int tmp;
+        (*files)[i] = WriteBlock(i*block_size + buff, &tmp);
+        (*files)[i]->start_pos_content = tmp;
+    }
+
+    *blocks -= min(*blocks, tree_size);
+}
+
+int small_pow(int a, int b) {
+    int res = a;
+    for(int i = 1; i < b; ++i) {
+        res *= a;
+    }
+    return res;
+}
+
+void FillLevelTree(int level, int* blocks, char* buff, struct block1024* parent) {
+    int bucket_size = *blocks;
+    bucket_size /= small_pow(tree_size, level);
+    if (*blocks % small_pow(tree_size, level) != 0) {
+        ++bucket_size;
+    }
+    parent->size = min(tree_size, bucket_size);
+    parent->children = (struct block1024**)malloc(parent->size*sizeof(struct block1024*));
+    for (int i = 0; i < parent->size; ++i) {
+        parent->children[i] = (struct block1024*)malloc(sizeof(struct block1024));
+        if(level == 1) {
+            parent->children[i]->children = NULL;
+            parent->children[i]->size = min(*blocks, tree_size);
+            FillTree(&parent->children[i]->files, blocks, buff + i * tree_size * block_size);
+        } else {
+            FillLevelTree(level - 1, blocks, buff + tree_size*tree_size * i * block_size,
+                          parent->children[i]);
+        }
+    }
+}
+
+struct fileTree* Write(char* content) {
+    struct fileTree* res = (struct fileTree*)malloc(sizeof(struct fileTree));
+
+    int blocks = strlen(content)/block_size;
+    if(strlen(content)%block_size != 0) {
+        ++blocks;
+    }
+    res->files = (struct file**)malloc(min(12, blocks)*sizeof(struct file*));
+    for(int i = 0; i < min(12, blocks); ++i) {
+        int tmp;
+        res->files[i] = WriteBlock(content + i*block_size, &tmp);
+        res->files[i]->start_pos_content = tmp;
+    }
+
+    res->size = min(12, blocks);
+
+    blocks -=12;
+
+    if(blocks <= 0) {
+        return res;
+    }
+
+    int size_tree = 1;
+    if (blocks > small_pow(tree_size, 1)) {
+        ++size_tree;
+    }
+    if (blocks > small_pow(tree_size, 2)) {
+        ++size_tree;
+    }
+    res->size += size_tree;
+    res->block_tree = (struct block1024**)malloc(size_tree*sizeof(struct block1024*));
+    res->block_tree[0] = (struct block1024*) malloc(sizeof(struct block1024));
+    res->block_tree[0]->size = min(blocks, tree_size);
+    FillTree(&res->block_tree[0]->files, &blocks, content + 12*block_size);
+    if (blocks <= 0) {
+        return res;
+    }
+
+    int summary_size = tree_size;
+    for (int i = 1; i < 3; ++i) {
+        res->block_tree[i] = (struct block1024*) malloc(sizeof(struct block1024));
+        FillLevelTree(i, &blocks, content + (12 + summary_size)*block_size, res->block_tree[i]);
+        if (blocks <= 0) {
+            return res;
+        }
+        summary_size*=(tree_size + 1);
+    }
+
+    assert(0);
+    return NULL;
+}
+
+struct fileTree* WriteDir(char* dir_name) {
+//    return Write(dir_name);
+    if(strlen(dir_name) > block_size) {
+        char msg[] = "Error! File name should be less \n";
+        send(sock, msg, sizeof(msg), 0);
+//        printf("Error! File name should be less then %d", block_size);
+        return NULL;
+    }
+
+    int tmp;
+    struct file* name = WriteBlock(dir_name, &tmp);
+    name->start_pos_name = tmp;
+
+    struct fileTree* res = (struct fileTree*)malloc(sizeof(struct fileTree));
+    res->file_name = name;
+    res->size = 0;
+    return res;
+}
+
+
+struct fileTree* WriteFile(char* file_name, char* file) {
+    if(strlen(file_name) > block_size) {
+        char msg[] = "Error! File name should be less \n";
+        send(sock, msg, sizeof(msg), 0);
+//        printf("Error! File name should be less then %d", block_size);
+        return NULL;
+    }
+
+    int tmp;
+    struct file* name = WriteBlock(file_name, &tmp);
+    name->start_pos_name = tmp;
+
+    struct fileTree* res = Write(file);
+    res->file_name = name;
+
+    free(file_name);
+    free(file);
+    return res;
+}
---
+++ main.c
@@ -0,0 +1,308 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <zconf.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "main.h"
+#include "iNode.h"
+#include "file.h"
+
+
+int* free_ranges;
+int free_ranges_size = 0;
+int sock;
+
+void AppendFreeRanges(int begin) { //todo x2
+    free_ranges = (int *)realloc(free_ranges, (free_ranges_size + 1)* sizeof(int));
+    free_ranges[free_ranges_size] = begin;
+    ++free_ranges_size;
+}
+
+int ClosestPos() {
+    if (free_ranges_size <= 0) {
+        return -1;
+    }
+    int res = free_ranges[free_ranges_size - 1];
+    free_ranges = (int *)realloc(free_ranges, (free_ranges_size - 1)* sizeof(int));
+    --free_ranges_size;
+    return res;
+}
+
+char* GetArg(char* buff, char delimeter) {
+    char* res = NULL;
+
+    for(int i = 0; i < strlen(buff); ++i) {
+        if (buff[i] == delimeter || buff[i] == '\n') {
+            res = (char*)malloc(i + 1);
+            strncpy(res, buff, i);
+            res[i] = '\0';
+            break;
+        }
+    }
+
+    if(res == NULL) {
+        return buff;
+    }
+    return res;
+}
+
+char* GetCommand(char* buff) {
+    int i = 0;
+    for(; i < strlen(buff); ++i) {
+        if(buff[i] == ' ' || buff[i] == '\n') {
+            break;
+        }
+    }
+    char* ans = (char*)malloc(i + 1);
+    strncpy(ans, buff, i);
+    ans[i] = 0;
+    return ans;
+}
+
+void Touch(struct iNode* folder, char* buff) {
+    char* filename;
+    char* file;
+
+    filename = GetArg(buff, ' ');
+
+    if (strlen(filename) == 0) {
+        char msg[] = "please add content... \n";
+        send(sock, msg, sizeof(msg), 0);
+        return;
+    }
+
+    file = GetArg(buff + strlen(filename) + 1, '\n');
+
+    if(strlen(file) == 0) {
+        char msg[] = "please add content... \n";
+        send(sock, msg, sizeof(msg), 0);
+        return;
+    }
+
+    struct fileTree* res = WriteFile(filename, file);
+    AddNewChild(folder, res);
+    char msg[] = "ok \n";
+    send(sock, msg, sizeof(msg), 0);
+}
+
+struct iNode* Cd(struct iNode* root, char* buff) {
+    char* addr = GetArg(buff, '\n');
+
+    if (strlen(addr) == 0) {
+        char msg[] = "no such directory \n";
+        send(sock, msg, sizeof(msg), 0);
+
+        return root;
+    }
+
+    char* part = GetArg(addr, '/');
+    int shift = 0;
+
+    struct iNode* res = root;
+    int param = 1;
+    while(strlen(part) > 0) {
+        shift += strlen(part);
+        res = Find(res, part, &param);
+        if (res == NULL) {
+            char msg[] = "no such directory \n";
+            send(sock, msg, sizeof(msg), 0);
+
+            return root;
+        }
+        part = GetArg(addr + shift, '/');
+    }
+
+    char msg[] = "ok \n";
+    send(sock, msg, sizeof(msg), 0);
+
+    return res;
+}
+
+void Ls(struct iNode* folder, char* buff) {
+    char* filename = GetArg(buff, '\n');
+    if (strlen(filename) != 0) {
+        int param = 0;
+        struct iNode* res = Find(folder, filename, &param);
+        if (res == NULL) {
+            char msg[] = "File is not found \n";
+            send(sock, msg, sizeof(msg), 0);
+        } else {
+            char* info = GetContentNew(res->file);
+            send(sock, info, strlen(info), 0);
+            free(info);
+        }
+    } else {
+        PrintChildren(folder);
+    }
+}
+
+struct iNode* Up(struct iNode* root) {
+    if (root->parent == NULL) {
+        char msg[] = "Impossible \n";
+        send(sock, msg, sizeof(msg), 0);
+//        printf("Impossible \n");
+        return root;
+    }
+    char msg[] = "ok \n";
+    send(sock, msg, sizeof(msg), 0);
+    return root->parent;
+}
+
+void DeleteElem(struct iNode* sample, int ind) {
+    //todo x2
+    struct iNode** new = (struct iNode**)malloc((sample->size)* sizeof(struct iNode*));
+
+    int j = 0;
+    for(int i = 0; i < sample->size; ++i) {
+        if (i != ind) {
+            new[i] = sample->children[j];
+        } else {
+            ind = -1;
+            --i;
+        }
+        ++j;
+    }
+    free(sample->children);
+    sample->children = new;
+}
+
+void Rm(struct iNode* folder, char* buff) {
+    char* filename = GetArg(buff, '\n');
+    int ind = -1;
+    struct iNode* res = Find(folder, filename, &ind);
+    if(res == NULL) {
+        char msg[] = "Not found \n";
+        send(sock, msg, sizeof(msg), 0);
+//        printf("Not found \n");
+        return;
+    }
+    DeleteChild(res);
+    DeleteElem(folder, ind);
+    if(folder->size == 0) {
+        folder->children = NULL;
+    }
+    char msg[] = "ok \n";
+    send(sock, msg, sizeof(msg), 0);
+}
+
+struct iNode* MakeDir(struct iNode* parent, char* buffer) {
+    char* dir_name = GetArg(buffer, '\n');
+    if (strlen(dir_name) == 0) {
+        char msg[] = "not empty dir please! \n";
+        send(sock, msg, sizeof(msg), 0);
+        return parent;
+    }
+    struct fileTree* file = WriteDir(dir_name);
+    struct iNode* res = AddNewChild(parent, file);
+    char msg[] = "ok \n";
+    send(sock, msg, sizeof(msg), 0);
+    return res;
+}
+
+void daemonize() {
+    pid_t pid;
+    pid = fork();
+
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    pid = fork();
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    umask(0);
+    chdir("/tmp");
+
+    for (int i = sysconf(_SC_OPEN_MAX); i >= 0; --i) {
+        close(i);
+    }
+
+    openlog("Daemon", LOG_PID, LOG_DAEMON);
+}
+
+void process(char* buff, struct iNode** root) {
+    char* command = GetCommand(buff);
+
+    if (strcmp(command, "touch") == 0) {
+        Touch(*root, buff + strlen(command) + 1);
+    } else if (strcmp(command, "makedir") == 0) {
+        MakeDir(*root, buff + strlen(command) + 1);
+    } else if (strcmp(command, "cd") == 0) {
+        *root = Cd(*root, buff + strlen(command) + 1);
+    } else if (strcmp(command, "ls") == 0) {
+        Ls(*root, buff + strlen(command) + 1);
+    } else if (strcmp(command, "..") == 0) {
+        *root = Up(*root);
+    } else if (strcmp(command, "rm") == 0) {
+        Rm(*root, buff + strlen(command) + 1);
+    } else {
+        char msg[] = "No such command... \n";
+        send(sock, msg, sizeof(msg), 0);
+    }
+
+    free(command);
+}
+
+int main() {
+    daemonize();
+    int size = BUFFER_SIZE;
+    char* buff = (char*)malloc(size);
+    int listener, bytes_read;
+    struct sockaddr_in addr;
+
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(5000);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(listener, (struct sockaddr *)&addr, sizeof(addr));
+    listen(listener, 1);
+
+    struct iNode* root = (struct iNode*)malloc(sizeof(struct iNode));
+    root->children = NULL;
+    root->parent = NULL;
+    char* root_name = (char*)malloc(2);
+    root_name[0] = '/';
+    root_name[1] = '\0';
+    root->file = WriteDir(root_name);
+
+    while(1) {
+        sock = accept(listener, NULL, NULL);
+        printf("Accepted... \n");
+        while(1) {
+            bytes_read = recv(sock, buff, size, 0);
+
+            if(bytes_read <= 0) {
+                break;
+            }
+
+            printf("%s", buff);
+            process(buff, &root);
+        }
+        close(sock);
+    }
+
+    free(free_ranges);
+    free(buff);
+
+    syslog (LOG_NOTICE, "Daemon terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
